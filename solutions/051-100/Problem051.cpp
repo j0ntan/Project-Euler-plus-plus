eabf87d882250fa61caa3e6e2b1b06e88618ce90
j0ntan <jgamboa0209@gmail.com>
2019-04-14 03:10:32 -0400

    Problem051: add problem statement & basic setup

---
+++ solutions/051-100/Problem051.cpp
@@ -0,0 +1,32 @@
+/* Prime digit replacements
+ *
+ * By replacing the 1st digit of the 2-digit number *3, it turns out that six of
+ * the nine possible values: 13, 23, 43, 53, 73, and 83, are all prime.
+ *
+ * By replacing the 3rd and 4th digits of 56**3 with the same digit, this
+ * 5-digit number is the first example having seven primes among the ten
+ * generated numbers, yielding the family: 56003, 56113, 56333, 56443, 56663,
+ * 56773, and 56993. Consequently 56003, being the first member of this family,
+ * is the smallest prime with this property.
+ *
+ * Find the smallest prime which, by replacing part of the number (not
+ * necessarily adjacent digits) with the same digit, is part of an eight prime
+ * value family. */
+
+#include <iostream>
+#include <vector>
+
+int main() {
+  //
+}
+
+/* Thought process:
+ * 1. It is not clear whether this smallest prime will be less-than or
+ * greater-than 56003. Therefore, as a first attempt we can try a brute force
+ * approach using values below 56003. A lower bound can be set as 10, the
+ * smallest 2-digit number.
+ * 2. Assuming we want to replace ONLY two numbers, we will choose two digits at
+ * index i and j. Because we want to replace two numbers, i and j must differ.
+ * For a chosen set of i and j, swapping their values will result in a duplicate
+ * replacement. Thus, we always want i < j.
+ * */
